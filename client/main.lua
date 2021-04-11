0aa339e1a9dec8ef1b4b3a684eb1a49397f1636b
GhzGarage <qbcore@programmer.net>
2021-04-10 22:48:23 -0500

    Keys Stuff

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,3 @@
-local Keys = {
-    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-}
 QBCore = nil
 
 Citizen.CreateThread(function()
@@ -35,7 +25,7 @@ Citizen.CreateThread(function ()
         ---- BUITEN
         if #(pos - vector3(Config['delivery'].outsideLocation.x, Config['delivery'].outsideLocation.y, Config['delivery'].outsideLocation.z)) < 1.3 then
             DrawText3D(Config['delivery'].outsideLocation.x, Config['delivery'].outsideLocation.y, Config['delivery'].outsideLocation.z + 1, "~g~E~w~ - To Enter")
-            if IsControlJustReleased(0, Keys["E"]) then
+            if IsControlJustReleased(0, 38) then
                 DoScreenFadeOut(500)
                 while not IsScreenFadedOut() do
                     Citizen.Wait(10)
@@ -48,7 +38,7 @@ Citizen.CreateThread(function ()
 
             if #(pos - vector3(Config['delivery'].insideLocation.x, Config['delivery'].insideLocation.y, Config['delivery'].insideLocation.z)) < 1.3 then
                 DrawText3D(Config['delivery'].insideLocation.x, Config['delivery'].insideLocation.y, Config['delivery'].insideLocation.z + 1, "~g~E~w~ - To Go Outside")
-                if IsControlJustReleased(0, Keys["E"]) then
+                if IsControlJustReleased(0, 38) then
                     DoScreenFadeOut(500)
                     while not IsScreenFadedOut() do
                         Citizen.Wait(10)
@@ -67,7 +57,7 @@ Citizen.CreateThread(function ()
                 else
                     DrawText3D(-1032.27,456.71,6.74 + 1, "~g~E~w~ -  Clock In")
                 end
-                if IsControlJustReleased(0, Keys["E"]) then
+                if IsControlJustReleased(0, 38) then
                     onDuty = not onDuty
                     if onDuty then
                         QBCore.Functions.Notify("You Have Been Clocked In")
@@ -103,7 +93,7 @@ Citizen.CreateThread(function ()
                 if carryPackage == nil then
                     if #(pos - vector3(packagePos.x, packagePos.y, packagePos.z)) < 2.3 then
                         DrawText3D(packagePos.x,packagePos.y,packagePos.z+ 1, "~g~E~w~ - Pack Package")
-                        if IsControlJustReleased(0, Keys["E"]) then
+                        if IsControlJustReleased(0, 38) then
                             QBCore.Functions.Progressbar("pickup_reycle_package", "Pick Up The Package ..", math.random(4000, 6000), false, true, {
                                 disableMovement = true,
                                 disableCarMovement = true,
@@ -120,7 +110,7 @@ Citizen.CreateThread(function ()
                 else
                     if #(pos - vector3(Config['delivery'].dropLocation.x, Config['delivery'].dropLocation.y, Config['delivery'].dropLocation.z)) < 2.0 then
                         DrawText3D(Config['delivery'].dropLocation.x, Config['delivery'].dropLocation.y, Config['delivery'].dropLocation.z, "~g~E~w~ - Hand In The Package")
-                        if IsControlJustReleased(0, Keys["E"]) then
+                        if IsControlJustReleased(0, 38) then
                             DropPackage()
                             ScrapAnim()
                             QBCore.Functions.Progressbar("deliver_reycle_package", "Unpacking The Package", 5000, false, true, {
