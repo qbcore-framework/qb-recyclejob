92f4b5f4ec5f4c39d27bf36a928aa647beed7305
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-11-08 01:48:35 +0200

    cleanup

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,7 @@
 local carryPackage = nil
 local onDuty = false
 
-Citizen.CreateThread(function ()
+CreateThread(function ()
     local RecycleBlip = AddBlipForCoord(Config['delivery'].outsideLocation.x, Config['delivery'].outsideLocation.y, Config['delivery'].outsideLocation.z)
     SetBlipSprite(RecycleBlip, 365)
     SetBlipColour(RecycleBlip, 2)
@@ -10,36 +10,35 @@ Citizen.CreateThread(function ()
     BeginTextCommandSetBlipName("STRING")
     AddTextComponentString("Recycle Center")
     EndTextCommandSetBlipName(RecycleBlip)
+	
     while true do
-        Citizen.Wait(0)
+        Wait(0)
         local pos = GetEntityCoords(PlayerPedId(), true)
-        ---- BUITEN
+
         if #(pos - vector3(Config['delivery'].outsideLocation.x, Config['delivery'].outsideLocation.y, Config['delivery'].outsideLocation.z)) < 1.3 then
             DrawText3D(Config['delivery'].outsideLocation.x, Config['delivery'].outsideLocation.y, Config['delivery'].outsideLocation.z + 1, "~g~E~w~ - To Enter")
             if IsControlJustReleased(0, 38) then
                 DoScreenFadeOut(500)
                 while not IsScreenFadedOut() do
-                    Citizen.Wait(10)
+                    Wait(10)
                 end
                 SetEntityCoords(PlayerPedId(), Config['delivery'].insideLocation.x, Config['delivery'].insideLocation.y, Config['delivery'].insideLocation.z)
                 DoScreenFadeIn(500)
             end
         end
-        ---- BINNEN
 
-            if #(pos - vector3(Config['delivery'].insideLocation.x, Config['delivery'].insideLocation.y, Config['delivery'].insideLocation.z)) < 1.3 then
-                DrawText3D(Config['delivery'].insideLocation.x, Config['delivery'].insideLocation.y, Config['delivery'].insideLocation.z + 1, "~g~E~w~ - To Go Outside")
-                if IsControlJustReleased(0, 38) then
-                    DoScreenFadeOut(500)
-                    while not IsScreenFadedOut() do
-                        Citizen.Wait(10)
-                    end
-                    SetEntityCoords(PlayerPedId(), Config['delivery'].outsideLocation.x, Config['delivery'].outsideLocation.y, Config['delivery'].outsideLocation.z + 1)
-                    DoScreenFadeIn(500)
-                end
-            end
-     
-        ---- INKLOKKEN
+		if #(pos - vector3(Config['delivery'].insideLocation.x, Config['delivery'].insideLocation.y, Config['delivery'].insideLocation.z)) < 1.3 then
+			DrawText3D(Config['delivery'].insideLocation.x, Config['delivery'].insideLocation.y, Config['delivery'].insideLocation.z + 1, "~g~E~w~ - To Go Outside")
+			if IsControlJustReleased(0, 38) then
+				DoScreenFadeOut(500)
+				while not IsScreenFadedOut() do
+					Wait(10)
+				end
+				SetEntityCoords(PlayerPedId(), Config['delivery'].outsideLocation.x, Config['delivery'].outsideLocation.y, Config['delivery'].outsideLocation.z + 1)
+				DoScreenFadeIn(500)
+			end
+		end
+
         if #(pos - vector3(1049.15, -3100.63, -39.95)) < 15 and not IsPedInAnyVehicle(PlayerPedId(), false) and carryPackage == nil then
             DrawMarker(2, 1049.15, -3100.63, -39.20, 0.9, 0, 0, 0, 0, 0, 0.2001, 0.2001, 0.2001, 255, 255, 255, 255, 0, 0, 0, 0)
             if #(pos - vector3(1049.15, -3100.63, -39.95)) < 1.3 then
@@ -62,17 +61,19 @@ Citizen.CreateThread(function ()
 end)
 
 local packagePos = nil
-Citizen.CreateThread(function ()
+
+CreateThread(function ()
     for k, pickuploc in pairs(Config['delivery'].pickupLocations) do
         local model = GetHashKey(Config['delivery'].warehouseObjects[math.random(1, #Config['delivery'].warehouseObjects)])
         RequestModel(model)
-        while not HasModelLoaded(model) do Citizen.Wait(0) end
+        while not HasModelLoaded(model) do Wait(0) end
         local obj = CreateObject(model, pickuploc.x, pickuploc.y, pickuploc.z, false, true, true)
         PlaceObjectOnGroundProperly(obj)
         FreezeEntityPosition(obj, true)
     end
+	
     while true do
-        Citizen.Wait(5)
+        Wait(5)
         if onDuty then
             if packagePos ~= nil then
                 local pos = GetEntityCoords(PlayerPedId(), true)
@@ -126,10 +127,10 @@ function ScrapAnim()
     loadAnimDict("mp_car_bomb")
     TaskPlayAnim(PlayerPedId(), "mp_car_bomb", "car_bomb_mechanic" ,3.0, 3.0, -1, 16, 0, false, false, false)
     openingDoor = true
-    Citizen.CreateThread(function()
+    CreateThread(function()
         while openingDoor do
             TaskPlayAnim(PlayerPedId(), "mp_car_bomb", "car_bomb_mechanic", 3.0, 3.0, -1, 16, 0, 0, 0, 0)
-            Citizen.Wait(1000)
+            Wait(1000)
             time = time - 1
             if time <= 0 then
                 openingDoor = false
@@ -142,7 +143,7 @@ end
 function loadAnimDict(dict)
     while (not HasAnimDictLoaded(dict)) do
         RequestAnimDict(dict)
-        Citizen.Wait(5)
+        Wait(5)
     end
 end
 
@@ -173,12 +174,12 @@ function PickupPackage()
     local pos = GetEntityCoords(PlayerPedId(), true)
     RequestAnimDict("anim@heists@box_carry@")
     while (not HasAnimDictLoaded("anim@heists@box_carry@")) do
-        Citizen.Wait(7)
+        Wait(7)
     end
     TaskPlayAnim(PlayerPedId(), "anim@heists@box_carry@" ,"idle", 5.0, -1, -1, 50, 0, false, false, false)
     local model = `prop_cs_cardbox_01`
     RequestModel(model)
-    while not HasModelLoaded(model) do Citizen.Wait(0) end
+    while not HasModelLoaded(model) do Wait(0) end
     local object = CreateObject(model, pos.x, pos.y, pos.z, true, true, true)
     AttachEntityToEntity(object, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 57005), 0.05, 0.1, -0.3, 300.0, 250.0, 20.0, true, true, false, true, 1, true)
     carryPackage = object
