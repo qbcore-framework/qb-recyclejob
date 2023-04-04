a37a832af08bfd08954d8ea258775114b3593b6f
AstroBaby1 <77403585+AstroBaby1@users.noreply.github.com>
2023-04-04 15:29:43 +0000

    Edit To main.lua
    Renamed Player to player for consistency with the rest of the code.
    Changed _ to i in the for loop for clarity.
    Changed math.random(1, #Config.ItemTable) to math.random(#Config.ItemTable) since the lower bound is always 1.
    Simplified the condition for the chance item to use math.random(100).
    Simplified the condition for the lucky item to use math.random(10) == math.random(10) instead of generating two separate random numbers.

--- server/main.lua
+++ server/main.lua
@@ -1,29 +1,25 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Events
-
 RegisterNetEvent('qb-recycle:server:getItem', function()
-  local src = source
-  local Player = QBCore.Functions.GetPlayer(src)
-  for _ = 1, math.random(1, Config.MaxItemsReceived), 1 do
-    local randItem = Config.ItemTable[math.random(1, #Config.ItemTable)]
-    local amount = math.random(Config.MinItemReceivedQty, Config.MaxItemReceivedQty)
-    Player.Functions.AddItem(randItem, amount)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[randItem], 'add')
-    Wait(500)
-  end
+    local src = source
+    local player = QBCore.Functions.GetPlayer(src)
 
-  local chance = math.random(1, 100)
-  if chance < 7 then
-    Player.Functions.AddItem(Config.ChanceItem, 1, false)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.ChanceItem], 'add')
-  end
+    for i = 1, math.random(1, Config.MaxItemsReceived) do
+        local randItem = Config.ItemTable[math.random(#Config.ItemTable)]
+        local amount = math.random(Config.MinItemReceivedQty, Config.MaxItemReceivedQty)
+        player.Functions.AddItem(randItem, amount)
+        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[randItem], 'add')
+        Wait(500)
+    end
 
-  local luck = math.random(1, 10)
-  local odd = math.random(1, 10)
-  if luck == odd then
-    local random = math.random(1, 3)
-    Player.Functions.AddItem(Config.LuckyItem, random)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.LuckyItem], 'add')
-  end
+    if math.random(100) < 7 then
+        player.Functions.AddItem(Config.ChanceItem, 1, false)
+        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.ChanceItem], 'add')
+    end
+
+    if math.random(10) == math.random(10) then
+        local random = math.random(1, 3)
+        player.Functions.AddItem(Config.LuckyItem, random)
+        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.LuckyItem], 'add')
+    end
 end)
