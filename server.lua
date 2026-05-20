853168d5b136c8f92287d32498749f0043d16d80
Kakarot <gigahertzgarage@mail.com>
2026-05-19 21:07:47 -0500

    Don't import whole core

--- server.lua
+++ server.lua
@@ -1,24 +1,25 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports['qb-core']:GetCoreObject({ 'Functions' })
+local sharedItems = exports['qb-core']:GetShared('Items')
 
 local Recieve = {
-    {item = 'metalscrap', min = 1, max = 5},
-    {item = 'plastic', min = 1, max = 5},
-    {item = 'copper', min = 1, max = 5},
-    {item = 'rubber', min = 1, max = 5},
-    {item = 'iron', min = 1, max = 5},
-    {item = 'aluminum', min = 1, max = 5},
-    {item = 'steel', min = 1, max = 5},
-    {item = 'glass', min = 1, max = 5},
+    { item = 'metalscrap', min = 1, max = 5 },
+    { item = 'plastic',    min = 1, max = 5 },
+    { item = 'copper',     min = 1, max = 5 },
+    { item = 'rubber',     min = 1, max = 5 },
+    { item = 'iron',       min = 1, max = 5 },
+    { item = 'aluminum',   min = 1, max = 5 },
+    { item = 'steel',      min = 1, max = 5 },
+    { item = 'glass',      min = 1, max = 5 },
 }
 local luckyItem = 'cryptostick' -- Item to be given as a lucky item
-local maxRecieved = 5 -- Max items to be received
+local maxRecieved = 5           -- Max items to be received
 local dropLocation = Config.DropLocation
-local LuckyItemChance = 20 -- 20% chance to get a lucky item
+local LuckyItemChance = 20      -- 20% chance to get a lucky item
 local uhohs = {}
 local Sales, Stock, salesLoc = {}, {}, Config.SellPed
 
 
-if Config.SellMaterials then 
+if Config.SellMaterials then
     Sales = { -- key is item, value is price
         metalscrap = 2,
         plastic = 2,
@@ -30,7 +31,7 @@ if Config.SellMaterials then
         glass = 2,
     }
 end
-if Config.LimitedMaterials then 
+if Config.LimitedMaterials then
     Stock = { -- key is item, value is stock at restart
         metalscrap = 3000,
         plastic = 3000,
@@ -62,7 +63,7 @@ end
 
 local function isClose(source, loc)
     local playerPed = GetPlayerPed(source)
-    local Player = QBCore.Functions.GetPlayer(source)
+    local Player = exports['qb-core']:GetPlayer(source)
     local cid = Player.PlayerData.citizenid
     local playerCoords = GetEntityCoords(playerPed)
     local distance = nil
@@ -106,22 +107,22 @@ local function checkStock(source, item, amount)
     if Stock[item] >= amount then
         return true
     else
-        TriggerClientEvent('QBCore:Notify', source, Lang:t('error.out_of_stock', {item = item}), 'error')
+        TriggerClientEvent('QBCore:Notify', source, Lang:t('error.out_of_stock', { item = item }), 'error')
         return false
     end
 end
 
 local function sellMaterials(src, item, amount)
-    local Player = QBCore.Functions.GetPlayer(src)
+    local Player = exports['qb-core']:GetPlayer(src)
     local price = Sales[item] * amount
-    local has = Player.Functions.GetItemByName(item)
+    local has = Player.GetItemByName(item)
     if has and has.amount < amount then
         amount = has.amount
         price = Sales[item] * amount
     end
-    if Player.Functions.RemoveItem(item, amount) then
-        Player.Functions.AddMoney('cash', price)
-        TriggerClientEvent('QBCore:Notify', src, Lang:t('success.sold', {amount = amount, item = QBCore.Shared.Items[item].label, price = price}), 'success')
+    if Player.RemoveItem(item, amount) then
+        Player.AddMoney('cash', price)
+        TriggerClientEvent('QBCore:Notify', src, Lang:t('success.sold', { amount = amount, item = sharedItems[item].label, price = price }), 'success')
         adjustStock(item, 'add', amount)
     else
         TriggerClientEvent('QBCore:Notify', src, Lang:t('error.nothing_to_sell'), 'error')
@@ -130,23 +131,26 @@ local function sellMaterials(src, item, amount)
 end
 
 local function getItem(source, item, amount)
-    local Player = QBCore.Functions.GetPlayer(source)
+    local Player = exports['qb-core']:GetPlayer(source)
     if Config.LimitedMaterials then
         if not checkStock(source, item, amount) then return end
-        Player.Functions.AddItem(item, amount)
-        TriggerClientEvent('qb-inventory:client:ItemBox', source, QBCore.Shared.Items[item], 'add', amount)
+        Player.AddItem(item, amount)
+        TriggerClientEvent('qb-inventory:client:ItemBox', source, sharedItems[item], 'add', amount)
         adjustStock(item, 'remove', amount)
     else
-        Player.Functions.AddItem(item, amount)
-        TriggerClientEvent('qb-inventory:client:ItemBox', source, QBCore.Shared.Items[item], 'add', amount)
+        Player.AddItem(item, amount)
+        TriggerClientEvent('qb-inventory:client:ItemBox', source, sharedItems[item], 'add', amount)
     end
 end
 
 RegisterNetEvent('qb-recyclejob:server:getItem', function()
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
+    local Player = exports['qb-core']:GetPlayer(src)
     if not isClose(src, 'turnIn') then
-        if not uhohs[src] then uhohs[src] = 1 return end
+        if not uhohs[src] then
+            uhohs[src] = 1
+            return
+        end
         uhohs[src] = uhohs[src] + 1 or 1
         if uhohs[src] >= 3 then
             exploitBan(src, 'Exploiting distance on qb-recyclejob')
@@ -166,15 +170,15 @@ RegisterNetEvent('qb-recyclejob:server:getItem', function()
     until itemAmountRecieved == 0
 
     local luckyChance = math.random(1, 100)
-    if luckyChance <= LuckyItemChance then 
-        Player.Functions.AddItem(luckyItem, 1)
-        TriggerClientEvent('qb-inventory:client:ItemBox', src, QBCore.Shared.Items[luckyItem], 'add', 1)
+    if luckyChance <= LuckyItemChance then
+        Player.AddItem(luckyItem, 1)
+        TriggerClientEvent('qb-inventory:client:ItemBox', src, sharedItems[luckyItem], 'add', 1)
     end
 end)
 
 RegisterNetEvent('qb-recyclejob:server:sellItem', function(item, amount)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
+    local Player = exports['qb-core']:GetPlayer(src)
     if not isClose(src, 'sell') then return end
     if not Sales[item] then return end
     if Config.SellMaterials then
